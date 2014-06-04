4fa881865faa864c2b836ad5cd070285a414291f
qatesting <healthqatesting@gmail.com>
2014-06-04 12:31:20 -0700

    Adding all the files

---
+++ pscwhite/PSCTest/PSCTest/tests/AddPatientTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using TestStack.White;
+using TestStack.White.UIItems.Finders;
+using PSCTest.core;
+using pscWindow = TestStack.White.UIItems.WindowItems.Window;
+using Application = TestStack.White.Application;
+using PSCTest.utilities;
+using Thread = System.Threading.Thread;
+using NUnit.Framework;
+
+
+namespace PSCTest.tests
+{
+    [TestFixture]
+    class AddPatientTest
+    {
+        //SampleTEst Class Variables
+        Application launchapp;
+        Application application = null;
+        static pscWindow currentWindow;
+        static SearchPage search = null;
+        StandardOperations standard;
+        Tabs tabs; 
+
+        public static void Main(string[] args)     { }
+
+        //SampleTest Class Constructor to launch PSC and get the current window of PSC
+        [TestFixtureSetUp]
+        public void SetupTest()
+        {            
+            launchapp = Setup.launchPSC();
+            application = Setup.attachPSC();
+            currentWindow = Setup.getWindow(application);
+            search = new SearchPage(currentWindow);
+            standard = new StandardOperations(currentWindow);
+            tabs = new Tabs(currentWindow);
+            //TestRunner runner = new TestRunner();            
+        }
+ 
+        [Test]
+        public void LoginPSC()
+        {
+            Login.loginPSC(currentWindow);
+            Thread.Sleep(5000);
+            Screenshot screen = new Screenshot();
+        }
+
+        [Test]
+        public void SearchAndAddPatient()
+        {
+            for (int patientid = 1; patientid < 9; patientid++)
+            {
+                search.SearchPatient(patientid);
+                Thread.Sleep(5000);
+                if (search.IsSearchEmpty())
+                {
+                    Console.WriteLine("Not able to search the patient, So adding the patient");
+                    search.AddNewPatient();
+                    AddNewPatientInformation(patientid);
+                }
+                else
+                {
+                    Console.WriteLine("Patient Found");
+                    search.SelectFirstSearchRecord();
+                    Thread.Sleep(10000);
+                    tabs.Dashboard();
+                    Thread.Sleep(2000);
+                    standard.Ok();
+                    Thread.Sleep(5000);
+                    continue;
+                }
+            }
+        }        
+ 
+        public void AddNewPatientInformation(int patientid)
+        {
+            Thread.Sleep(5000);
+            BasicInfoPage bip = new BasicInfoPage(currentWindow);
+            bip.ProvideBasicInformation(patientid);
+            Thread.Sleep(2000);
+            standard.Save();
+            Thread.Sleep(3000);
+            tabs.Dashboard();
+            Thread.Sleep(2000);
+            standard.Ok();
+        }
+    }
+ }
+
+
---
+++ pscwhite/PSCTest/PSCTest/tests/TestsStructure.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSCTest.tests
+{
+    class TestsStructure
+    {
+
+        /*SampleTest test = new SampleTest();
+            
+        Login.loginPSC(currentWindow);
+        Thread.Sleep(5000);
+        Screenshot screen = new Screenshot();
+        screen.GetScreenshot("temp.png");
+        search.SearchPatient(1);
+        Thread.Sleep(5000);
+        bool flag = search.IsSearchEmpty();
+        if (flag == true)
+        {
+            Console.WriteLine("Not able to search the patient, So adding the patient");
+            search.AddNewPatient();
+            test.ProvideNewPatientInformation();
+        }
+        else if (flag == false)
+        {
+            Console.WriteLine("Patient Found");
+            search.SelectFirstSearchRecord();
+        }
+        //test.InputAdditionInfo();
+        //test.AddGuardianInformation();
+        //test.PerformOperations();
+        //test.AddInsuranceInfo();
+        //test.AddNewPatient(); */
+
+
+        /*       public void InputAdditionInfo()
+                {
+                    AdditionalInfoPage aip = new AdditionalInfoPage(currentWindow);
+                    standard.Next();
+                    Thread.Sleep(5000);
+                    aip.ProvideMailingAddress(1);
+                    Thread.Sleep(2000);
+                    // aip.SameAsMailingAddress("No");
+                    // aip.ProvideBillingAddress(1);
+            
+                }
+
+                public void AddInsuranceInfo()
+                {
+                    for (int i = 0; i < 10; i++)
+                        standard.Back();
+
+                    Thread.Sleep(5000);
+                    AdditionalInfoPage aip = new AdditionalInfoPage(currentWindow);            
+                    Thread.Sleep(2000);
+                    aip.AddInsurance(1);
+                }
+
+                public void AddGuardianInformation()
+                {
+                    //standard.Next();
+                    standard.Next();
+                    Thread.Sleep(5000);
+                    AdditionalInfoPage aip = new AdditionalInfoPage(currentWindow);
+                    //aip.AddGuardian(1);
+                    aip.AddEmergencyContact(1);
+                    Thread.Sleep(1000);
+                    standard.Delete();
+                    Thread.Sleep(2000);
+                    standard.Ok();
+                    //standard.Delete();
+                }
+
+                public void PerformOperations()
+                {            
+                    //StandardOperations standard = new StandardOperations(currentWindow);
+                    Thread.Sleep(5000);
+                    tabs.Dashboard();
+                    Thread.Sleep(5000);
+                    standard.Cancel();
+                    Thread.Sleep(5000);
+                    tabs.Dashboard();
+                    Thread.Sleep(5000);
+                    standard.Ok();           
+                    Console.ReadLine();
+                }*/
+    }
+}
